ad3c78e6ed01d4ae40cdcf1257bc7d31e27c070a
卢晋辉 <coder_lujinhui@163.com>
2019-02-21 15:38:39 +0800

    代码格式化，添加README

--- lib/moblink_interface.dart
+++ lib/moblink_interface.dart
@@ -8,29 +8,30 @@ class Moblink {
   static const MethodChannel _channel =
       const MethodChannel('com.yoozoo.mob/moblink');
 
-  static Future<dynamic> getMobId(MLSDKScene scene, Function(String mobid, String domain, MLSDKError error) result)  {
-    
+  static Future<dynamic> getMobId(MLSDKScene scene,
+      Function(String mobid, String domain, MLSDKError error) result) {
     Map args = {"path": scene.path, "params": scene.params};
 
-    Future<dynamic> callback = _channel.invokeMethod(MobLinkMethods.getMobId.name, args);
+    Future<dynamic> callback =
+        _channel.invokeMethod(MobLinkMethods.getMobId.name, args);
 
     callback.then((dynamic response) {
       if (result != null) {
-        result(response["mobid"], 
-              response["domain"], 
-              MLSDKError(rawData: response["error"]));
+        result(response["mobid"], response["domain"],
+            MLSDKError(rawData: response["error"]));
       }
     });
     return callback;
   }
 
   static Future<dynamic> restoreScene(Function(MLSDKScene scene) result) {
+    Future<dynamic> callback =
+        _channel.invokeMethod(MobLinkMethods.restoreScene.name);
 
-    Future<dynamic> callback = _channel.invokeMethod(MobLinkMethods.restoreScene.name);
-    
     callback.then((dynamic response) {
       if (result != null) {
-        MLSDKScene scenes = new MLSDKScene(response["path"], response["params"]);
+        MLSDKScene scenes =
+            new MLSDKScene(response["path"], response["params"]);
         scenes.mobid = response["mobid"];
         scenes.className = response["className"];
         scenes.rawURL = response["rawURL"];
@@ -42,7 +43,6 @@ class Moblink {
 }
 
 class MLSDKScene {
-
   // path (required)
   String path;
   // custom parameter (Optional)
@@ -57,5 +57,4 @@ class MLSDKScene {
 
   // create scene
   MLSDKScene(this.path, this.params);
-
 }
